fe43d9a64c94d569cebf8f7f4cfc6205b941127c
KohlFische <kohlfische@gmail.com>
2023-12-11 19:45:53 +0800

    new file:   ZhuZiAYuanJWD.woff2 	new file:   favicon.ico 	new file:   index.html 	new file:   main.js 	new file:   readme.md 	new file:   style.css

---
+++ main.js
@@ -0,0 +1,104 @@
+window.onload = function () { alert("注意：专题题库查不到时可以选择全部题库/大一/大二的基础题库查询。专题题库是基础题库和专题题库混合抽题的。\n\n感谢 Rosemoe 提供的 api 接口以及 inko 提供的反代服务，有任何问题可加 QQ 群 601745455 咨询。\n\n最新题库已经更新。2023-12-11-19:36"); }
+
+let options = document.querySelectorAll('.option div');
+let optionBox = document.querySelector('.optionBox');
+
+options.forEach(function (option) {
+    option.addEventListener('click', function () {
+        optionBox.value = this.innerText;
+    })
+})
+
+let dropdown = document.querySelector('.dropdown');
+
+dropdown.addEventListener('click', function () {
+    dropdown.classList.toggle('active');
+})
+
+let form = document.querySelector('form');
+let searchBox = document.querySelector('.searchBox');
+let cardContainer = document.querySelector('.cardContainer');
+let title = document.querySelector('.title');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    if (encodeURIComponent(optionBox.value) == "%E5%85%A8%E9%83%A8%E9%A2%98%E5%BA%93")
+        var apiUrl = 'https://api-rosemoe-cyou.inko.lv/v1/problem_bank/query?problem_set=all&content_query=' + encodeURIComponent(searchBox.value);
+    else
+        var apiUrl = 'https://api-rosemoe-cyou.inko.lv/v1/problem_bank/query?problem_set=' + encodeURIComponent(optionBox.value) + '&content_query=' + encodeURIComponent(searchBox.value);
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', apiUrl, true);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                let response = JSON.parse(xhr.responseText);
+                if (response.success === true) {
+                    cardContainer.innerHTML = '';
+                    cardContainer.style.display = 'none';
+                    if (window.innerWidth < 900) {
+                        form.style.top = '150px';
+                        title.style.top = '100px';
+                    }
+                    else {
+                        form.style.top = '350px';
+                        title.style.top = '300px';
+                    }
+
+                    if (response.results.length > 0) {
+                        if (window.innerWidth < 900) {
+                            form.style.top = '40px';
+                            title.style.top = '20px';
+                        }
+                        else {
+                            form.style.top = '130px';
+                            title.style.top = '100px';
+                        }
+                        cardContainer.style.display = 'block';
+
+                        response.results.forEach(function (result) {
+                            let card = document.createElement('div');
+                            card.classList.add('card');
+
+                            let content = document.createElement('div');
+                            content.classList.add('content');
+                            content.textContent = result.content;
+
+                            let optionsList = document.createElement('div');
+                            optionsList.classList.add('optionsList');
+
+                            result.options.forEach(function (option) {
+                                let optionItem = document.createElement('div');
+                                optionItem.textContent = option.name + '. ' + option.content;
+
+                                optionsList.appendChild(optionItem);
+                            })
+
+                            let answer = document.createElement('div');
+                            answer.classList.add('answer');
+                            answer.textContent = '答案：' + result.answer;
+
+                            let line = document.createElement('div');
+                            line.classList.add('line');
+
+                            card.appendChild(content);
+                            card.appendChild(line.cloneNode(true));
+                            card.appendChild(optionsList);
+                            card.appendChild(line.cloneNode(true));
+                            card.appendChild(answer);
+                            cardContainer.appendChild(card);
+                        })
+                    }
+                    else alert('没有找到相关的题目！');
+                }
+                else if (response.message === 'forbidden: keyword should be at least 2 characters') alert('题目或关键词至少为2个字符！');
+                else if (response.message === 'forbidden: keyword can not contain brackets') alert('题目或关键词不能包含括号！');
+            }
+            else alert('API请求失败！请访问 https://ath.inko.lv 或加 QQ 群 601745455 咨询。');
+        }
+    }
+
+    xhr.send();
+})
---
+++ readme.md
@@ -0,0 +1,15 @@
+## 使用方法
+1. 点击进入[武汉理工大学体育理论在线查询题库](http://athletics.kohlfische.top/)
+2. 选择需要查询的题库<br><img src="https://m.qpic.cn/psc?/V51ahGj52TSm2G1HYFQ90MQmdC0oFr6P/ruAMsa53pVQWN7FLK88i5oKgWWr1W94mcXAbY8BBuuNnDYYRSHDbpu6FyW44m0JcvTvYhgTLHwLdV9bI.F*5cnqWlwfYICv9.B1CKomP3zk!/mnull&bo=GAFyAAAAAAADB0k!&rf=photolist&t=5">
+3. 输入题目或关键词<br><img src="https://m.qpic.cn/psc?/V51ahGj52TSm2G1HYFQ90MQmdC0oFr6P/ruAMsa53pVQWN7FLK88i5oKgWWr1W94mcXAbY8BBuuOKSec8IMvNwsBy4lETTiF8opDIjEPE2o7FajMRM9lopaQlBqpVFyj6Qi2u.ATcCXk!/mnull&bo=HgOKAAAAAAADB7U!&rf=photolist&t=5">
+3. 获得答案，进行答题
+
+## 注意事项
+专题题库查不到时可以选择全部题库/大一/大二的基础题库查询。专题题库是基础题库和专题题库混合抽题的。
+
+## 感谢
+感谢 Rosemoe 提供的 api 接口，有任何问题可加 QQ 群 601745455 咨询。
+
+可以请开发者们喝一杯咖啡么~<br>
+<img src="https://m.qpic.cn/psc?/V51ahGj52TSm2G1HYFQ90MQmdC0oFr6P/ruAMsa53pVQWN7FLK88i5oKgWWr1W94mcXAbY8BBuuPrVjqASzXJWlKY7b*BLaCQeOAwLAVKa5hkhfPWOPoP6.75BMZcqhxAl72eU2ZqSrQ!/mnull&bo=OASQBgAAAAABB4o!&rf=photolist&t=5">
+<img src="https://m.qpic.cn/psc?/V51ahGj52TSm2G1HYFQ90MQmdC0oFr6P/ruAMsa53pVQWN7FLK88i5oKgWWr1W94mcXAbY8BBuuPbc62T4CH7IEo3aDGibUXVL1lZVTjsgayazaYjVfRTHJwaEGRZnJ1nIXCsDVvgHDA!/mnull&bo=OASQBgAAAAABB4o!&rf=photolist&t=5">
